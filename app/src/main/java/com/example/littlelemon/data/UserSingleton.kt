7dc679ef990d69a1e40e4bc0f9a939838edf3166
Steve <stevebedard29@gmail.com>
2023-02-14 16:41:53 -0500

    Modified the singleton UserSingleton.kt

--- app/src/main/java/com/example/littlelemon/data/UserSingleton.kt
+++ app/src/main/java/com/example/littlelemon/data/UserSingleton.kt
@@ -1,8 +1,31 @@
 package com.example.littlelemon.data
 
+import android.content.Context
+
 object UserSingleton{
     var firstName: String? = null
     var lastName: String? = null
     var email: String? = null
     var isLoggedIn: Boolean = false
+
+    // Save the user data to SharedPreferences
+    fun saveToPreferences(context: Context) {
+        val sharedPref = context.getSharedPreferences("UserPrefs", Context.MODE_PRIVATE)
+        with(sharedPref.edit()) {
+            putString("firstName", firstName)
+            putString("lastName", lastName)
+            putString("email", email)
+            putBoolean("isLoggedIn", isLoggedIn)
+            apply()
+        }
+    }
+
+    // Load the user data from SharedPreferences
+    fun loadFromPreferences(context: Context) {
+        val sharedPref = context.getSharedPreferences("UserPrefs", Context.MODE_PRIVATE)
+        firstName = sharedPref.getString("firstName", null)
+        lastName = sharedPref.getString("lastName", null)
+        email = sharedPref.getString("email", null)
+        isLoggedIn = sharedPref.getBoolean("isLoggedIn", false)
+    }
 }
